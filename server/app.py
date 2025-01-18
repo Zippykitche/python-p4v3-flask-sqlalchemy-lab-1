8509d39a186820ca4ea718e0f91758c7d970fc0d
zippykitche <zippyk80@gmail.com>
2025-01-18 18:05:13 +0300

    flask sqlachemy lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,24 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)  
+    if earthquake:
+        return jsonify(earthquake.to_dict()) 
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    data = [earthquake.to_dict() for earthquake in earthquakes]
+
+    return jsonify({
+        "count": len(data),
+        "quakes": data
+    })
+
 
 
 if __name__ == '__main__':
